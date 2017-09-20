5e1e6fba9418c46939c7a662e99fe82de22e837c
Anupam Jain <ajnsit@gmail.com>
2017-09-20 13:54:47 +0530

    Remove MonadTransMap

--- concur-core/src/Control/MonadShiftMap.hs
+++ concur-core/src/Control/MonadShiftMap.hs
@@ -1,18 +1,23 @@
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RankNTypes            #-}
+{-# LANGUAGE TypeOperators         #-}
 module Control.MonadShiftMap where
 
-import           Control.MonadTransMap
+import           Control.Natural              (type (~>))
 
--- | Lateral mapping. Generalises `MonadTransMap`.
--- TODO: I'm sure some Lens concept maps to this
+import           Control.Monad.State          (StateT, mapStateT)
+import           Control.Monad.Trans.Identity (IdentityT, mapIdentityT)
+
+-- | Mapping between Natural Transformations
 class MonadShiftMap s t where
-  shiftMap :: (forall x. s x -> s x) -> t a -> t a
+  shiftMap :: (s ~> s) -> (t ~> t)
 
--- This is the primary reason for this. This cannot be done by MonadTrans.
 instance MonadShiftMap m m where
   shiftMap = id
 
-instance MonadTransMap t => MonadShiftMap m (t m) where
-  shiftMap = liftMap
+instance MonadShiftMap m (IdentityT m) where
+  shiftMap = mapIdentityT
+
+instance MonadShiftMap m (StateT s m) where
+  shiftMap = mapStateT
