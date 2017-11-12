b1e2d64caa5f30b04faaf351a5046d0c7507e027
Anupam Jain <ajnsit@gmail.com>
2017-11-12 11:21:32 +0530

    Switch to webpack for Concur-React

--- concur-react/build.sh
+++ concur-react/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-browserify jsbits/concur.js | uglifyjs > jsbits/concur.compiled.js
-stack build --stack-yaml stack-ghcjs.yaml --no-nix
+webpack
+stack build --stack-yaml stack-ghcjs.yaml
