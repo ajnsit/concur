51b807cc0e18edfc9fb09e937dfaa98cf1e4d048
Anupam Jain <ajnsit@gmail.com>
2018-08-02 13:20:55 +0530

    Rename MonadShiftMap to ShiftMap

--- concur-core/src/Control/ShiftMap.hs
+++ concur-core/src/Control/ShiftMap.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RankNTypes            #-}
 {-# LANGUAGE TypeOperators         #-}
-module Control.MonadShiftMap where
+module Control.ShiftMap where
 
 import           Control.Natural              (type (~>))
 
@@ -10,14 +10,14 @@ import           Control.Monad.State          (StateT, mapStateT)
 import           Control.Monad.Trans.Identity (IdentityT, mapIdentityT)
 
 -- | Mapping between Natural Transformations
-class MonadShiftMap s t where
+class ShiftMap s t where
   shiftMap :: (s ~> s) -> (t ~> t)
 
-instance MonadShiftMap m m where
+instance ShiftMap m m where
   shiftMap = id
 
-instance MonadShiftMap m (IdentityT m) where
+instance ShiftMap m (IdentityT m) where
   shiftMap = mapIdentityT
 
-instance MonadShiftMap m (StateT s m) where
+instance ShiftMap m (StateT s m) where
   shiftMap = mapStateT
