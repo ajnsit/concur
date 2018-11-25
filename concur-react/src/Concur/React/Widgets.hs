85639a60aa8cae873a6c6b17119f3d4e8390f7e3
Stefano Debenedetti <44903077+demaledetti@users.noreply.github.com>
2018-11-25 23:52:45 +0100

    Align with API change introduced by 51b807cc0e18edfc9fb09e937dfaa98cf1e4d048 (Rename MonadShiftMap to ShiftMap)

--- concur-react/src/Concur/React/Widgets.hs
+++ concur-react/src/Concur/React/Widgets.hs
@@ -16,7 +16,7 @@ import           Control.Concurrent.STM     (STM, atomically)
 import           Unsafe.Coerce              (unsafeCoerce)
 
 import           Concur.React.DOM
-import           Control.MonadShiftMap
+import           Control.ShiftMap
 import           Control.MonadSTM
 
 -- TODO: Since we can't have a top level text in React. We currently always wrap it in span.
@@ -24,15 +24,15 @@ text :: String -> Widget HTML a
 text txt = display [vtext $ JSS.pack txt]
 
 -- Like el_ but accepts a React Component reference instead of a tagname
-elComp :: (MonadShiftMap (Widget HTML) m) => JSVal -> [VAttr] -> m a -> m a
+elComp :: (ShiftMap (Widget HTML) m) => JSVal -> [VAttr] -> m a -> m a
 elComp e attrs = shiftMap (wrapView (vnode e attrs))
 
 -- Generic Element wrapper (single child widget)
-el_ :: (MonadShiftMap (Widget HTML) m) => JSString -> [VAttr] -> m a -> m a
+el_ :: (ShiftMap (Widget HTML) m) => JSString -> [VAttr] -> m a -> m a
 el_ = elComp . unsafeCoerce
 
 -- Generic Element wrapper
-el :: (MonadShiftMap (Widget HTML) m, MultiAlternative m) => JSString -> [VAttr] -> [m a] -> m a
+el :: (ShiftMap (Widget HTML) m, MultiAlternative m) => JSString -> [VAttr] -> [m a] -> m a
 el e attrs = el_ e attrs . orr
 
 -- Create a dom leaf node
@@ -48,7 +48,7 @@ mkEventHandlerAttr evtName = do
   return (attr, await n)
 
 -- Handle arbitrary events on an element.
-elEvent :: (MonadShiftMap (Widget HTML) m, MultiAlternative m, Monad m, MonadSTM m)
+elEvent :: (ShiftMap (Widget HTML) m, MultiAlternative m, Monad m, MonadSTM m)
         => JSString
         -> (JSVal -> a)
         -> JSString
@@ -58,7 +58,7 @@ elEvent :: (MonadShiftMap (Widget HTML) m, MultiAlternative m, Monad m, MonadSTM
 elEvent evtName xtract tag attrs child = elEventComp evtName xtract (unsafeCoerce tag) attrs child
 
 -- Like elEvent, but works for arbitrary react components
-elEventComp :: (MonadShiftMap (Widget HTML) m, MultiAlternative m, Monad m, MonadSTM m)
+elEventComp :: (ShiftMap (Widget HTML) m, MultiAlternative m, Monad m, MonadSTM m)
         => JSString
         -> (JSVal -> a)
         -> JSVal
@@ -70,7 +70,7 @@ elEventComp evtName xtract e attrs child = do
   orr [fmap xtract $ liftSTM w, elComp e (a:attrs) child]
 
 -- Similar to elEventComp, but specialised to the case where there are no child events.
-elEventComp' :: (MonadShiftMap (Widget HTML) m, MultiAlternative m, Monad m, MonadSTM m)
+elEventComp' :: (ShiftMap (Widget HTML) m, MultiAlternative m, Monad m, MonadSTM m)
          => JSString
          -> JSVal
          -> [VAttr]
@@ -79,7 +79,7 @@ elEventComp' :: (MonadShiftMap (Widget HTML) m, MultiAlternative m, Monad m, Mon
 elEventComp' evtName comp attrs child = elEventComp evtName id comp attrs (fmap absurd child)
 
 -- Similar to elEvent, but specialised to the case where there are no child events.
-elEvent' :: (MonadShiftMap (Widget HTML) m, MultiAlternative m, Monad m, MonadSTM m)
+elEvent' :: (ShiftMap (Widget HTML) m, MultiAlternative m, Monad m, MonadSTM m)
          => JSString
          -> JSString
          -> [VAttr]
@@ -88,11 +88,11 @@ elEvent' :: (MonadShiftMap (Widget HTML) m, MultiAlternative m, Monad m, MonadST
 elEvent' evtName tag attrs child = elEvent evtName id tag attrs (fmap absurd child)
 
 -- A clickable button widget. Returns either the button click, or the child event.
-button :: (MonadShiftMap (Widget HTML) m, MultiAlternative m, Monad m, MonadSTM m) => [VAttr] -> m () -> m ()
+button :: (ShiftMap (Widget HTML) m, MultiAlternative m, Monad m, MonadSTM m) => [VAttr] -> m () -> m ()
 button attrs w = elEvent "onClick" (const ()) "button" attrs w
 
 -- A clickable button widget, specialised to the case when there are no child events.
-button' :: (MonadShiftMap (Widget HTML) m, MultiAlternative m, Monad m, MonadSTM m) => [VAttr] -> m Void -> m ()
+button' :: (ShiftMap (Widget HTML) m, MultiAlternative m, Monad m, MonadSTM m) => [VAttr] -> m Void -> m ()
 button' attrs w = void $ elEvent' "onClick" "button" attrs w
 
 -- Text input. Returns the contents on keypress enter.
